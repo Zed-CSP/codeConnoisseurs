5bb231a7c7ade83fa556804887b1d7eefe53381b
Riley O'Neil <roneilwork@gmail.com>
2023-05-17 15:31:30 -0700

    added user seeds, fixed a route in Ingredients

--- routes/api/ingredientRoutes.js
+++ routes/api/ingredientRoutes.js
@@ -11,4 +11,13 @@ router.post('/', async (req,res) => {
     }   
 })
 
+router.get('/', async (req, res) => {
+    try {
+        const ingredientData = await Ingredient.findAll();
+        res.status(200).json(ingredientData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,18 @@
 const seedIngredients = require('./ingredient-seeds');
+const seedUsers = require('./user-seeds');
 
 const sequelize = require('../config/connection');
+
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
     await seedIngredients();
     console.log('\n ----- INGREDIENTS SEEDED -----\n');
+    await seedUsers();
+    console.log('\n ----- USERS SEEDED -----\n');
 
     process.exit(0);
 };
+
+seedAll();
