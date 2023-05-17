687368406db4d17cb798c4dbbe88553820038768
Riley O'Neil <roneilwork@gmail.com>
2023-05-17 16:45:32 -0700

    using test methods to check login/logout

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,22 +1,23 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//api/users endpoint
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
-        .status(400)
+        .status(402)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword =  userData.checkPassword(req.body.password);
 
     if (!validPassword) {
       res
-        .status(400)
+        .status(403)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
@@ -29,9 +30,33 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
+
+//FOR TESTING PURPOSES ONLY
+router.post('/test', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(402)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    const validPassword =  userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(403)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    res.json({ user: userData, message: 'You are now logged in!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -7,28 +7,28 @@ const userData = [
         last_name: 'Roos',
         email: 'roos@email.com',
         password: 'password12345'
-    }
+    },
     {
         id: '3593569f-6fec-4de6-a1ab-0662085366aa',
         first_name: 'Chris',
         last_name: 'Peret',
         email: 'peret@email.com',
         password: 'password12345'
-    }
+    },
     {
         id: '3590669f-60ec-4de6-a1ab-0662085394bb',
         first_name: 'Phuong',
         last_name: 'To',
         email: 'to@email.com',
         password: 'password12345'
-    }
+    },
     {
         id: '3234569f-60ec-4de6-a1ab-0662085366aa',
         first_name: 'Riley',
         last_name: 'ONeil',
         email: 'oneil@email.com',
         password: 'password12345'
-    }
+    },
     {
         id: '3590669f-60ec-4de6-a1ab-0662085395aa',
         first_name: 'Sarah',
