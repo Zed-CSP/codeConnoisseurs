11ac800ad4d59d10b1844f32bf057da7b20f8585
sendusyourbones <beckee.roos@gmail.com>
2023-05-17 17:37:45 -0700

    Fix user seeds so password hashing works on create

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -38,5 +38,5 @@ const userData = [
     }
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () => User.bulkCreate(userData, { individualHooks: true });
 module.exports = seedUsers;
