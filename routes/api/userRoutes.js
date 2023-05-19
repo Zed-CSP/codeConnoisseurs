d423901fbb5a99948184c3ade3c3c437e5afef05
Christopher <123341169+Zed-CSP@users.noreply.github.com>
2023-05-18 19:39:06 -0700

    Update userRoutes.js
    removed testing earmark comment

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -26,7 +26,6 @@ router.post('/login', async (req, res) => {
   }
 });
 
-//FOR TESTING PURPOSES ONLY
 router.post('/test', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
