003bff1522bc139c2d2a561b5c0e8155acf2c9f6
Riley O'Neil <roneilwork@gmail.com>
2023-05-18 20:38:01 -0700

    fixed signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,11 +1,12 @@
-signUpForm.addEventListener('submit', async (e) => {
+document.querySelector('#submit').addEventListener('click', async (e) => {
     e.preventDefault();
-    const formData = new FormData(form);
+    const formData = new FormData(document.querySelector('#signup-form'));
     const first_name = formData.get('firstName');
     const last_name = formData.get('lastName');
     const email = formData.get('email');
     const password = formData.get('password');
     const body = {first_name, last_name, email, password};
+    console.log(first_name, last_name, email, password, body);
 
     try {
       const res = await fetch('/api/users/signup', {
@@ -15,6 +16,11 @@ signUpForm.addEventListener('submit', async (e) => {
       });
       const data = await res.json();
       console.log(data);
+      if(res.ok) {
+        document.location.replace('/recipe/add');
+      } else {
+        alert(data.message);
+      }
       } catch (error) {
       console.error(error);
       }
