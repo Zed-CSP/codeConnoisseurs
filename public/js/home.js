6095d7015c8eaf4c839689af690b0d9a1ec3f22c
Christopher Peret <cperet89@gmail.com>
2023-05-19 19:11:43 -0700

    clicking on feed item goes to recipe route but is currently unfinished

--- public/js/home.js
+++ public/js/home.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const postCards = document.querySelectorAll('.post-card');
   
     postCards.forEach((card) => {
-      card.addEventListener('click', () => {
-        const postId = card.id;
-        // ADD ROUTE TO POST PAGE
-        console.log('Clicked on post:', postId);
-      });
+        card.addEventListener('click', () => {
+            const postId = card.id;
+            console.log('Clicked on post:', postId);
+            window.location.href = `/recipe/${postId}`
+        });
     });
-  });
+});
